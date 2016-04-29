fae09c720cdfa04b43cbcf92c71288547a24c04b
Anders Tornøe <ato@kvuc.dk>
2016-04-29 09:16:09 +0200

    autoCommit by terminal

--- tinder.js
+++ tinder.js
@@ -75,7 +75,7 @@ function generateHTML() {
         if (opg_type == "problemformulering") {
             $(".tinder_card").eq(i).html("<p class='card_header'><b>Nøgleproblem:</b> " + jsonData[i].Nogleproblem + "</p><p class='card_text'><b>Problemformulering:</b> ''" + jsonData[i].Problemformulering + "''</p><div class='txt_vurdering'></div>");
         } else {
-            $(".tinder_card").eq(i).html("<p class='card_header card_header_konkl'><b>Nøgleproblem:</b> " + jsonData[i].Nogleproblem + "</p><p class='card_text_konkl'><b>Problemformulering:</b> ''" + jsonData[i].Problemformulering + "''</p><p class='card_text_konkl'><b>Konklusion: </b>''" + jsonData[i].Konklusion + "''</p><div class='txt_vurdering'></div>");
+            $(".tinder_card").eq(i).html("<p class='card_header card_header_konkl'><b>Problemformulering:</b> ''" + jsonData[i].Problemformulering + "''</p><p class='card_text_konkl'></p><p class='card_text_konkl'><b>Konklusion: </b>''" + jsonData[i].Konklusion + "''</p><div class='txt_vurdering'></div>");
         }
         $(".tinder_card").eq(i).css("z-index", 20 - i);
         $(".tinder_card").eq(i).css("margin-top", i * 7);
